4e9419452b03f71992036c4aa524099ff7c1130c
Hdsan <hudsonferreira2501@gmail.com>
2023-03-06 18:38:30 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
